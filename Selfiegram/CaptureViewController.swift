b97bf72e0f5395cc75523fcf006be5c3fcb9ac61
Anil <farisolusa@gmail.com>
2018-11-17 00:57:58 +0530

    Custom CaptureView Done

--- Selfiegram/CaptureViewController.swift
+++ Selfiegram/CaptureViewController.swift
@@ -9,28 +9,6 @@
 import UIKit
 import AVKit
 
-class CaptureViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    override func viewWillLayoutSubviews() {
-        //self.cameraPreview?.setCameraOrientation(currentVideoOrientation)
-    }
-}
-
 class PreviewView: UIView {
     var previewLayer: AVCaptureVideoPreviewLayer?
     
@@ -63,5 +41,115 @@ class PreviewView: UIView {
     
     func setCameraOrientation(_ orientation: AVCaptureVideoOrientation) {
         previewLayer?.connection?.videoOrientation = orientation
+    }
+}
+
+class CaptureViewController: UIViewController {
+    
+    @IBOutlet weak var cameraPreview: PreviewView!
+    typealias ComplitionHandler = (UIImage?) -> Void
+    var complition: ComplitionHandler?
+    let captureSession = AVCaptureSession()
+    let photoOutput = AVCapturePhotoOutput()
+    
+    var currentVideoOrientation: AVCaptureVideoOrientation {
+        let orientationMap: [UIDeviceOrientation: AVCaptureVideoOrientation]
+        
+        orientationMap = [
+            .portrait: .portrait,
+            .landscapeLeft: .landscapeLeft,
+            .landscapeRight: .landscapeRight,
+            .portraitUpsideDown: .portraitUpsideDown
+        ]
+        
+        let currentOrientation = UIDevice.current.orientation
+        let videoOrientation = orientationMap[currentOrientation, default: .portrait]
+        
+        return videoOrientation
+    }
+    
+    override func viewDidLoad() {
+        
+        let discovery = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
+            mediaType: AVMediaType.video,
+            position: AVCaptureDevice.Position.front
+        )
+        
+        // Get the first available devide, bail if we can't find one
+        
+        guard let captureDevice = discovery.devices.first else {
+            NSLog("No capture devices available")
+            self.complition?(nil)
+            return
+        }
+        
+        // Attempt to add this device to the capture session
+        do {
+            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        } catch {
+            NSLog("Failed to add camera to capture session: \(error)")
+            self.complition?(nil)
+        }
+        
+        // Configure the camera to use high-resolution capture settings
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        // Begin the capture session
+        captureSession.startRunning()
+        
+        // Add the photo output to the session, so that it can receive photos when it wants them
+        if captureSession.canAddOutput(photoOutput) {
+            captureSession.addOutput(photoOutput)
+        }
+        
+        self.cameraPreview.setSession(captureSession)
+        
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        self.complition?(nil)
+    }
+    
+    #warning ("Not attached to any action")
+    @IBAction func takeSelfie(_ sender: Any) {
+        // Get a connection to the output
+        guard let videoConnection = photoOutput.connection(with: AVMediaType.video) else {
+            print("Failed to get camera connection")
+            return
+        }
+        
+        // Set its orientation, so that the image is oriented correctly
+        videoConnection.videoOrientation = currentVideoOrientation
+        
+        // Indicate that we want the data it captures to be in JPEG format
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        
+        // Begin capturing a photo; it will call
+        // photoOutput(_ didFinishProcessingPhoto:, error:) when done
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+ 
+    override func viewWillLayoutSubviews() {
+        self.cameraPreview?.setCameraOrientation(currentVideoOrientation)
+    }
+}
+
+
+extension CaptureViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            print("Failed to get photo: \(error)")
+            return
+        }
+        
+        guard let jpegData = photo.fileDataRepresentation(),
+        let image = UIImage(data: jpegData) else {
+            print("Failed to get image from encoded data")
+            return
+        }
+        
+        self.complition?(image)
     }
 }
