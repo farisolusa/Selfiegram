f88e5432426886b35b01cd27d515d6fe2b9a66d8
Anil <farisolusa@gmail.com>
2018-11-20 04:06:41 +0530

    DataLoadingTests-I

---
+++ SelfiegramTests/DataLoadingTests.swift
@@ -0,0 +1,94 @@
+//
+//  DataLoadingTests.swift
+//  SelfiegramTests
+//
+//  Created by Anil Prasad on 11/20/18.
+//  Copyright © 2018 Anil Prasad. All rights reserved.
+//
+
+import XCTest
+@testable import Selfiegram
+
+class DataLoadingTests: XCTestCase {
+
+    override func setUp() {
+        // Remove all cached data
+        let cachedURL = OverlayManager.cacheDirectoryURL
+        
+        guard let contents = try? FileManager.default.contentsOfDirectory(at: cachedURL, includingPropertiesForKeys: nil, options: []) else {
+            XCTFail("Failed to list contents of directory \(cachedURL)")
+            return
+        }
+        
+        var complete = true
+        for file in contents {
+            do {
+                try FileManager.default.removeItem(at: file)
+            } catch {
+                print("Test setup: Failed to remove item \(file); \(error)")
+                complete = false
+            }
+        }
+        if !complete {
+            XCTFail("Failed to delete contents of cache")
+        }
+    }
+    
+    func testNoOverlaysAvailable() {
+        // Arrange
+        // Nothing to arrange here: our start condition is that there is no cached data
+        
+        // Act
+        let availableOverlays = OverlayManager.shared.availableOverlays()
+        
+        // Assert
+        XCTAssertEqual(availableOverlays.count, 0)
+    }
+    
+    func testGettingOverlayInfo() {
+        // Arrange
+        let expectation = self.expectation(description: "Done downloading")
+        
+        // Act
+        var loadedInfo: OverlayManager.OverlayList?
+        var loadedError: Error?
+        
+        OverlayManager.shared.refreshOverlays { (info, error) in
+            loadedInfo = info
+            loadedError = error
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+        
+        // Assert
+        XCTAssertNotNil(loadedInfo)
+        XCTAssertNil(loadedError)
+    }
+    
+    // The Overlay manager can download overlay assets, making them available for use
+    func testDownloadingOverlays() {
+        // Arrange
+        let loadingComplete = self.expectation(description: "Download done")
+        var availableOverlays: [Overlay] = []
+        
+        // Act
+        OverlayManager.shared.loadOverlayAssets(refresh: true) {
+            availableOverlays = OverlayManager.shared.availableOverlays()
+            loadingComplete.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+        
+        // Assert
+        XCTAssertNotEqual(availableOverlays.count, 0)
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    
+
+}
